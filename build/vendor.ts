132292b245e3b5d01d51d9307fd610ba6a835c10
Jubal Mabaquiao <jmmabaquiao@multisyscorp.com>
2022-10-05 18:12:28 +0800

    inject new vendor dependencies

--- build/vendor.ts
+++ build/vendor.ts
@@ -9,7 +9,7 @@ const dependencyPaths = [
 	{ packageName: 'es-module-shims', subfolderToVendor: 'dist', entrypointFile: 'es-module-shims.js' },
 	{ packageName: 'preact', subfolderToVendor: 'dist', entrypointFile: 'preact.module.js' },
 	{ packageName: 'preact/jsx-runtime', subfolderToVendor: 'dist', entrypointFile: 'jsxRuntime.module.js' },
-	{ packageName: 'preact/hooks', subfolderToVendor: 'dist', entrypointFile: 'hooks.module.js' },
+	{ packageName: 'preact/hooks', subfolderToVendor: 'dist', entrypointFile: 'hooks.module.js' }, { packageName: 'ethers', subfolderToVendor: 'dist', entrypointFile: 'ethers.esm.js', }, { packageName: '@preact/signals-core', subfolderToVendor: 'dist', entrypointFile: 'signals-core.module.js', }, { packageName: '@preact/signals', subfolderToVendor: 'dist', entrypointFile: 'signals.module.js' }
 ]
 
 async function vendorDependencies() {

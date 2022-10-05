b7af3532401c994d815ef04705232dd916b5cb8f
Jubal Mabaquiao <jmmabaquiao@multisyscorp.com>
2022-10-05 18:16:12 +0800

    initial implementation of wallet context

---
+++ app/ts/components/Balance.tsx
@@ -0,0 +1,17 @@
+import { useWallet } from './WalletProvider';
+
+export const Balance = () => {
+	const { account, status, balance, checkBalance } = useWallet();
+
+	if (!account.value) return null;
+
+	if (status.value === 'BUSY') return <div>Balance: ...</div>;
+
+	if (!balance.value) return <div>Balance:</div>;
+
+	return (
+		<div>
+			Balance: {balance} <button onClick={() => checkBalance()}>🔄</button>
+		</div>
+	);
+};

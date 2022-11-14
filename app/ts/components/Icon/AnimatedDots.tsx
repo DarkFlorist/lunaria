c52d87910f9b8396d575825cb682480bac23e7ca
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2022-11-14 15:09:34 +0800

    introduced icons

---
+++ app/ts/components/Icon/AnimatedDots.tsx
@@ -0,0 +1,15 @@
+export default () => (
+	<>
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='currentColor'
+		>
+			<circle class='animate-fade-dots' cx='4' cy='12' r='3' />
+			<circle class='animate-fade-dots' cx='12' cy='12' r='3' />
+			<circle class='animate-fade-dots' cx='20' cy='12' r='3' />
+		</svg>
+	</>
+);
---
+++ app/ts/components/Icon/index.ts
@@ -0,0 +1,20 @@
+import ArrowRight from './ArrowRight'
+import Check from './Check'
+import Copy from './Copy'
+import Ethereum from './Ethereum'
+import Loading from './AnimatedDots'
+import Refresh from './Refresh'
+import Xmark from './Xmark'
+
+export default Object.assign(
+	{},
+	{
+		ArrowRight,
+		Check,
+		Copy,
+		Ethereum,
+		Loading,
+		Refresh,
+		Xmark,
+	}
+)

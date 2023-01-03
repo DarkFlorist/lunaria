2d8cdbfa655adc1c791c1a02ff8760e94229d0ac
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-01-03 09:20:59 +0800

    remove initial implementation

--- app/ts/index.tsx
+++ app/ts/index.tsx
@@ -1,11 +1,4 @@
 import { render } from 'preact'
 import { App } from './components/App.js'
-import { WalletProvider } from './components/WalletProvider.js'
 
-render(
-	<WalletProvider>
-		<App />
-	</WalletProvider>,
-	document.body,
-	document.querySelector('main') as HTMLElement
-)
+render(<App />, document.body, document.querySelector('main') as HTMLElement)

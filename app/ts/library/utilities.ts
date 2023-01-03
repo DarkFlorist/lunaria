f830c144474faccc5adb0fd1e684cb889ce0d54a
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-01-03 11:08:13 +0800

    create shared store for account states

--- app/ts/library/utilities.ts
+++ app/ts/library/utilities.ts
@@ -1,3 +1,5 @@
+import { ethers } from 'ethers'
+
 export async function sleep(milliseconds: number) {
 	await new Promise((resolve) => setTimeout(resolve, milliseconds))
 }
@@ -19,4 +21,31 @@ export function removeNonStringsAndTrim(...strings: (string | boolean | undefine
 			// combine strings separated by spaces
 			.join(' ')
 	)
+}
+
+/**
+ * Check string for valid address format
+ */
+export function assertsAddress(string: string): asserts string is `0x${string}` {
+	if (!/^0x[0-9A-Fa-f]{40}$/.test(string)) throw new Error(`Invalid address ${string}`)
+}
+
+export function isAddress(string: string): string is `0x${string}` {
+	return /^0x[0-9A-Fa-f]{40}$/.test(string)
+}
+
+/**
+ * Describe a window ethereum object
+ */
+type ExternalProvider = ethers.providers.ExternalProvider
+export function assertsExternalProvider(ethereum: unknown): asserts ethereum is ExternalProvider {
+	if (ethereum === null || ethereum === undefined || typeof ethereum !== 'object') throw new Error('Ethereum object does not exist')
+}
+
+export type EthereumWithListeners = {
+	on(eventName: string | symbol, listener: (...args: any[]) => void): void
+}
+
+export function isEthereumObservable(ethereum: unknown): ethereum is EthereumWithListeners {
+	return ethereum instanceof Object && 'on' in ethereum && typeof ethereum.on === 'function'
 }
---
+++ app/ts/store/account.ts
@@ -0,0 +1,79 @@
+import { effect, signal } from '@preact/signals'
+import { ethers } from 'ethers'
+import { isMetaMaskRpcError, MetaMaskRpcError } from '../library/exceptions.js'
+import { assertsAddress, assertsExternalProvider, isEthereumObservable, isAddress } from '../library/utilities.js'
+
+type AccountBusy = {
+	status: 'busy'
+}
+
+type AccountConnected = {
+	status: 'connected'
+	address: string
+}
+
+type AccountDisconnected = {
+	status: 'disconnected'
+	connect: () => void
+	ensureConnected: () => void
+}
+
+type AccountConnectRejected = {
+	status: 'rejected'
+	error: MetaMaskRpcError | Error
+}
+
+export type AccountStore = AccountBusy | AccountConnected | AccountDisconnected | AccountConnectRejected
+
+const storeDefaults = { status: 'disconnected', connect, ensureConnected } as const
+const store = signal<AccountStore>(storeDefaults)
+export const accountStore = store
+
+async function connect() {
+	try {
+		assertsExternalProvider(window.ethereum)
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		store.value = { status: 'busy' }
+		const accounts = await provider.send('eth_requestAccounts', [])
+		assertsAddress(accounts[0])
+		store.value = { status: 'connected', address: accounts[0] }
+	} catch (exception) {
+		let error = new Error(`An unknown error was encountered ${exception}`)
+		if (exception instanceof Error) {
+			error = exception
+		}
+		if (typeof exception === 'string') {
+			error = new Error(exception)
+		}
+		if (exception instanceof Object) {
+			if (isMetaMaskRpcError(exception)) {
+				error = new MetaMaskRpcError(exception)
+			}
+		}
+		store.value = { status: 'rejected', error }
+	}
+}
+
+async function ensureConnected() {
+	assertsExternalProvider(window.ethereum)
+	const provider = new ethers.providers.Web3Provider(window.ethereum)
+	store.value = { status: 'busy' }
+	const accounts = await provider.listAccounts()
+	store.value = isAddress(accounts[0]) ? { status: 'connected', address: accounts[0] } : storeDefaults
+}
+
+const handleAccountChange = (newAccount: string[]) => {
+	if (store.value?.status !== 'connected') return
+	store.value = isAddress(newAccount[0]) ? { status: 'connected', address: newAccount[0] } : storeDefaults
+}
+
+// watch for account changes when an account is connected
+const observeAccountStatusChanges = () => {
+	if (!isEthereumObservable(window.ethereum)) return
+	window.ethereum.on('accountsChanged', handleAccountChange)
+}
+
+effect(() => {
+	if (store.value.status !== 'connected') return
+	observeAccountStatusChanges()
+})

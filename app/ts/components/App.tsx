c620c868438dfd1f2cbea5e3329ba3bbd049f051
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-01-17 12:54:40 +0800

    Hash Routing (#43)
    * create hashrouter component
    * require handler in hashchange effect
    * capture dynamic params in hash
    * update example removing hashbang
    * update VNode assertion
      - Co-authored-by: Micah Zoltu <micah@zoltu.net>
    * more performant reduce
      - Co-authored-by: Micah Zoltu <micah@zoltu.net>
    * fix missing closing brace
      - Co-authored-by: Micah Zoltu <micah@zoltu.net>

--- app/ts/components/App.tsx
+++ app/ts/components/App.tsx
@@ -1,10 +1,47 @@
-import { Route, Router } from './Router.js'
-import { SendEthPage } from './SendEthPage.js'
+import { Route, Router, useParams } from './HashRouter.js'
 
 export function App() {
 	return (
 		<Router>
-			<Route path='/' component={SendEthPage} />
+			<Route path='#send' component={Send} />
+			<Route path='#tx/:transaction_hash' component={Transaction} />
+			<Route path='/' component={Home} />
 		</Router>
+	)
+}
+
+const Home = () => {
+	return (
+		<div class='px-4'>
+			<h4 class='my-3'>Home Page</h4>
+			<a class='px-4 py-2 border' href='#send'>
+				Go to send page
+			</a>
+		</div>
+	)
+}
+
+const Send = () => {
+	return (
+		<div class='px-4'>
+			<h4 class='my-3'>Send Page</h4>
+			<a class='px-4 py-2 border' href='#tx/43ll'>
+				Simulate send transaction
+			</a>
+		</div>
+	)
+}
+
+const Transaction = () => {
+	const params = useParams()
+	const hash = params && 'transaction_hash' in params && params.transaction_hash
+
+	return (
+		<div class='px-4'>
+			<h4 class='my-3'>Transaction Details</h4>
+			<div>
+				Transaction Hash: <strong>{hash}</strong>
+			</div>
+		</div>
 	)
 }
---
+++ app/ts/components/HashRouter.tsx
@@ -0,0 +1,82 @@
+import { signal } from '@preact/signals'
+import { FunctionalComponent, VNode } from 'preact'
+import { useEffect } from 'preact/hooks'
+
+export type RouteProps = {
+	path: string
+	component: FunctionalComponent
+}
+
+export const Route = ({ component: Component }: RouteProps) => <Component />
+
+export type RouterProps = {
+	children: object[]
+	onHashChange?: (event: Event) => void
+}
+
+const activeRoute = signal<VNode<RouteProps> | undefined>(undefined)
+
+export const Router = ({ children, onHashChange }: RouterProps) => {
+	function getActiveRoute() {
+		return children.filter(isRouteComponent).find(child => matchPattern(child.props.path, window.location.hash))
+	}
+
+	activeRoute.value = getActiveRoute()
+
+	useHashChangeEffect((event: Event) => {
+		activeRoute.value = getActiveRoute()
+		onHashChange?.(event)
+	})
+
+	return <main>{activeRoute.value}</main>
+}
+
+export function useParams() {
+	if (!activeRoute.value) return
+	return matchPath(activeRoute.value.props.path, window.location.hash)
+}
+
+function useHashChangeEffect(handler: (event: Event) => void) {
+	useEffect(() => {
+		window.addEventListener('hashchange', handler)
+		return () => window.removeEventListener('hashchange', handler)
+	}, [])
+}
+
+function isRouteComponent(child: object): child is VNode<{ path: string }> {
+	return 'props' in child && typeof child.props === 'object' && child.props !== null && 'path' in child.props && typeof child.props.path === 'string'
+}
+
+function stripLeadingSlash(urlString: string) {
+	return urlString.charAt(0) !== '/' ? urlString : urlString.substring(1)
+}
+
+function matchPattern(pattern: string, str: string): boolean {
+	const patternParts = stripLeadingSlash(pattern).split('/')
+	const strParts = str.split('/')
+
+	if (patternParts.length !== strParts.length) return false
+
+	for (let i = 0; i < patternParts.length; i++) {
+		const patternPart = patternParts[i]
+		const strPart = strParts[i]
+		if (patternPart.startsWith(':')) continue
+		if (patternPart !== strPart) return false
+	}
+
+	return true
+}
+
+function matchPath(pattern: string, path: string): { [key: string]: string } | null {
+	const patternParts = stripLeadingSlash(pattern).split('/')
+	const pathParts = path.split('/')
+
+	if (patternParts.length !== pathParts.length) {
+		return null
+	}
+
+	return patternParts.reduce((params, part, index) => {
+		if (part.startsWith(':')) params[part.slice(1)] = pathParts[index]
+		return params
+	}, {} as { [key: string]: string })
+}

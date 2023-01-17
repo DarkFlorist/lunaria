d8bb504a1d56580c60a7605cf30579b178cb5376
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-01-17 13:20:50 +0800

    update send page ui layout

--- app/ts/components/ConnectToWallet.tsx
+++ app/ts/components/ConnectToWallet.tsx
@@ -1,6 +1,7 @@
-import { FunctionalComponent } from 'preact'
+import { ComponentChildren } from 'preact'
 import { EthereumJsonRpcError } from '../library/exceptions.js'
 import { accountStore } from '../store/account.js'
+import Blockie from './Blockie.js'
 import { Button } from './Button.js'
 import * as Icon from './Icon/index.js'
 
@@ -11,34 +12,39 @@ export const ConnectToWallet = () => {
 		case 'disconnected':
 			return (
 				<Wrapper>
-					<div class='leading-tight text-right'>Get started quickly by connecting your wallet</div>
-					<div class='transition animate-bounce-x'>
-						<Icon.ArrowRight />
+					<div class='grid grid-cols-[minmax(auto,max-content)_minmax(auto,max-content)_minmax(auto,max-content)] gap-3 items-center justify-center'>
+						<div class='leading-tight text-right'>Get started quickly by connecting your wallet</div>
+						<div class='transition animate-bounce-x'>
+							<Icon.ArrowRight />
+						</div>
+						<Button class='whitespace-nowrap' onClick={() => account.connect()}>
+							Connect Wallet
+						</Button>
 					</div>
-					<Button class='whitespace-nowrap' onClick={() => account.connect()}>
-						Connect Wallet
-					</Button>
 				</Wrapper>
 			)
 
 		case 'busy':
 			return (
 				<Wrapper>
-					<div class='flex flex-col items-end'>
+					<div class='grid md:grid-flow-col grid-cols-[minmax(min-content,max-content)_minmax(min-content,max-content)] grid-rows-[minmax(min-content,max-content)] gap-x-4 items-center justify-center md:place-items-end'>
+						<div class='h-10 w-10 animate-pulse bg-white/50 rounded row-span-2 md:order-last' />
 						<div class='h-3 w-24 animate-pulse bg-white/50 rounded-sm my-1' />
 						<div class='h-4 w-full min-w-[11rem] max-w-sm animate-pulse bg-white/50 rounded-sm my-1' />
 					</div>
-					<Button class='border border-white/30 py-2 px-4 rounded leading-tight text-white/50'>Connecting...</Button>
 				</Wrapper>
 			)
 
 		case 'connected':
 			return (
 				<Wrapper>
-					<div class='flex gap-4 min-w-0'>
-						<div class='text-right min-w-0'>
-							<div class='text-sm text-white/50'>Your Address</div>
-							<div class='overflow-hidden text-ellipsis'>{account.address}</div>
+					<div class='grid md:grid-flow-col grid-cols-[minmax(min-content,max-content)_minmax(auto,max-content)] grid-rows-[minmax(min-content,max-content)] gap-x-4 items-center justify-center md:place-items-end'>
+						<div class='row-span-2 md:order-last'>
+							<Blockie size={10} borderRadius='0.25em' seed={account.address} />
+						</div>
+						<div class='text-sm text-white/50'>Your Address</div>
+						<div class='overflow-hidden text-ellipsis'>
+							<span>{account.address}</span>
 						</div>
 					</div>
 				</Wrapper>
@@ -47,16 +53,12 @@ export const ConnectToWallet = () => {
 		case 'rejected':
 			if (account.error instanceof EthereumJsonRpcError) {
 				return (
-					<Wrapper>
-						<div class='text-center md:text-right'>
-							<div class='font-bold'>Failed to connect to wallet!</div>
-							<div class='text-sm text-white/50'>
-								<a class='flex items-center gap-1 italic' title={`${account.error.message} (${account.error.code})`}>
-									<span>Open your wallet extension window for details</span>️ <Icon.Info />
-								</a>
-							</div>
-						</div>
-					</Wrapper>
+					<div class='text-center md:text-right'>
+						<div class='font-bold'>Failed to connect to wallet!</div>
+						<a class='text-sm text-white/50 flex items-center justify-center gap-1 italic' title={`${account.error.message} (${account.error.code})`}>
+							<span>Open your wallet extension window for details</span>️ <Icon.Info />
+						</a>
+					</div>
 				)
 			}
 
@@ -73,10 +75,6 @@ export const ConnectToWallet = () => {
 	}
 }
 
-const Wrapper: FunctionalComponent = ({ children }) => {
-	return (
-		<div class='flex justify-center my-6 md:h-24 md:my-0'>
-			<div class='flex gap-4 items-center justify-center py-4 px-6 border border-dashed border-white/30 rounded md:border-0 min-w-0 max-w-xl md:max-w-none lg:max-w-none md:py-0'>{children}</div>
-		</div>
-	)
+const Wrapper = ({ children }: { children: ComponentChildren }) => {
+	return <div class='flex items-center justify-center border border-dashed border-white/30 md:border-0 rounded min-h-[4rem] md:min-h-0 px-3 md:px-0'>{children}</div>
 }

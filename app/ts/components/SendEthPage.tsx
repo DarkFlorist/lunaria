4711c30b8a9289f1193a4faa64b911b879efeb87
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-01-24 13:29:54 +0800

    init send functionality

--- app/ts/components/SendEthPage.tsx
+++ app/ts/components/SendEthPage.tsx
@@ -4,6 +4,9 @@ import { ConnectToWallet } from './ConnectToWallet.js'
 import { AddressField } from './AddressField.js'
 import { AmountField } from './AmountField.js'
 import { ComponentChildren } from 'preact'
+import { sendTransactionStore, TransactionComposing, TransactionFailed, TransactionStore } from '../store/transaction.js'
+import { accountStore } from '../store/account.js'
+import { useEffect } from 'preact/hooks'
 
 export const SendEthPage = () => {
 	return (
@@ -35,7 +38,6 @@ export const SendEthPage = () => {
 	)
 }
 
-
 export type SendTransactionInput = {
 	amount: string
 	to: string
@@ -46,30 +48,166 @@ const Page = ({ children }: { children: ComponentChildren }) => {
 		<Layout.Page class='grid [grid-template-areas:_"header"_"body"_"footer"] grid-rows-[minmax(min-content,max-content)_minmax(min-content,auto)_minmax(min-content,max-content)]'>{children} </Layout.Page>
 	)
 }
+
 const Header = ({ children }: { children: ComponentChildren }) => <Layout.Header class='grid [grid-template-areas:_"brand"_"profile"] md:[grid-template-areas:_"brand_profile"] md:items-center md:justify-between gap-4 py-4 px-6'>{children}</Layout.Header>
 
 const Body = ({ children }: { children: ComponentChildren }) => <Layout.Body class='grid gap-6 [grid-template-areas:_"essentials"_"main"_"aside"] md:[grid-template-areas:_"main_essentials"_"main_aside"] md:grid-cols-[3fr_2fr] xl:grid-cols-[5fr_2fr] md:grid-rows-[minmax(min-content,max-content)_1fr] px-6'>{children}</Layout.Body>
 
-const Main = () => <div class='grid [grid-template-areas:_"title"_"token"_"amount"_"address"_"tip"_"controls"] grid-rows-[repeat(auto-fit,minmax(min-content,0))] gap-y-4 lg:[grid-template-areas:_"title_title"_"token_amount"_"address_address"_"tip_tip"_"controls_controls"] lg:gap-x-6 xl:[grid-template-areas:_"title_title_title"_"token_amount_tip"_"address_address_tip"_"controls_controls_tip"]'>
-	<div class='[grid-area:title]'>
-		<div class='bg-white/10 text-xl font-bold px-6 py-2 -ml-6'>Send Funds</div>
-	</div>
-	<div class='[grid-area:token]'>
-		<TokenSelectField />
-	</div>
-	<div class='[grid-area:amount]'>
-		<AmountField value='0' onChange={() => { }} label='Amount' name='amount' />
-	</div>
-	<div class='[grid-area:address]'>
-		<AddressField value='' onChange={() => { }} label='Address' name='to' />
-	</div>
-	<div class='[grid-area:tip] border border-dashed border-white/30'>
-		<div class='p-4'>Guide</div>
-	</div>
-	<div class='[grid-area:controls]'>
-		<button class='px-4 py-2 hover:bg-white/10 border w-full'>Send</button>
-	</div>
-</div>
+const Main = () => {
+	return (
+		<SendForm>
+			<div class='grid [grid-template-areas:_"title"_"token"_"amount"_"address"_"tip"_"controls"] grid-rows-[repeat(auto-fit,minmax(min-content,0))] gap-y-4 lg:[grid-template-areas:_"title_title"_"token_amount"_"address_address"_"tip_tip"_"controls_controls"] lg:gap-x-6 xl:[grid-template-areas:_"title_title_title"_"token_amount_tip"_"address_address_tip"_"controls_controls_tip"] lg:grid-cols-2 xl:grid-cols-3'>
+				<div class='[grid-area:title]'>
+					<div class='bg-white/10 text-xl font-bold px-6 py-2 -ml-6'>Send Funds</div>
+				</div>
+				<div class='[grid-area:token]'>
+					<TokenSelectField />
+				</div>
+				<div class='[grid-area:amount]'>
+					<SendAmountField />
+				</div>
+				<div class='[grid-area:address]'>
+					<SendAddressField />
+				</div>
+				<div class='[grid-area:tip] border border-dashed border-white/30'>
+					<SendGuide />
+				</div>
+				<div class='[grid-area:controls]'>
+					<SendActions />
+				</div>
+			</div>
+		</SendForm>
+	)
+}
+
+
+const SendForm = ({ children }: { children: ComponentChildren }) => {
+	const txn = sendTransactionStore
+	const account = accountStore
+
+	function handleSubmit(event: Event) {
+		event.preventDefault()
+		switch (account.value.status) {
+			case 'busy':
+			case 'rejected':
+				return false
+			case 'disconnected':
+				return account.value.connect()
+			case 'connected': {
+				switch (txn.value.status) {
+					case 'composing':
+						return txn.value.send()
+					case 'signed':
+						return location.href = `#tx/${txn.value.transaction.hash}`
+					case 'failed':
+						return txn.value.reset()
+					default:
+						return false
+				}
+			}
+		}
+	}
+
+	useEffect(() => {
+		txn.value.status === 'idle' && txn.value.compose()
+	}, [])
+
+	return <form onSubmit={handleSubmit}>{children}</form>
+}
+
+const isEditInputAllowed = (store: TransactionStore): store is TransactionComposing | TransactionFailed =>
+	store.status === 'composing' || store.status === 'failed'
+
+const SendAmountField = () => {
+	const txn = sendTransactionStore
+
+	function handleChange(amount: string) {
+		if (!isEditInputAllowed(txn.value)) return;
+		txn.value.data = { ...txn.value.data, amount }
+		if (txn.value.status === 'failed') txn.value.reset()
+	}
+
+	const inputValue = txn.value.status === 'idle' ? '' : txn.value.data.amount
+	const disabled = !isEditInputAllowed(txn.value)
+
+	return <AmountField value={inputValue} onChange={handleChange} label='Amount' name='amount' disabled={disabled} />
+}
+
+const SendAddressField = () => {
+	const txn = sendTransactionStore
+
+	function handleChange(to: string) {
+		if (!isEditInputAllowed(txn.value)) return;
+		txn.value.data = { ...txn.value.data, to }
+		if (txn.value.status === 'failed') txn.value.reset()
+	}
+
+	const inputValue = txn.value.status === 'idle' ? '' : txn.value.data.to
+	const disabled = !isEditInputAllowed(txn.value)
+
+	return <AddressField value={inputValue} onChange={handleChange} label='Address' name='to' disabled={disabled} />
+}
+
+const SendGuide = () => {
+	const txn = sendTransactionStore
+
+	switch (txn.value.status) {
+		case 'composing':
+			return (
+				<div class='p-4 text-center xl:text-left'>
+					<div class='mb-2'>What happens when I click send?</div>
+					<div class='leading-tight text-white/50 text-sm'>This app will forward your request to the wallet you chose to connect with. The connected wallet handles signing and submitting your requst to the chain.</div>
+				</div>
+			)
+		case 'signing':
+			return (
+				<div class='p-4'>
+					<div class='mb-2 font-bold'>Awaiting wallet confirmation...</div>
+					<div class='leading-tight text-white/50 text-sm'>At this point, your connected wallet will need action to proceed with this transaction. Carefully check the information before accepting the wallet confirmation.</div>
+				</div>
+			)
+		case 'signed':
+			return <div class='p-4'>
+				<div class='mb-2 font-bold'>Request Successfully Sent!</div>
+				<div class='leading-tight text-white/50 text-sm'>Your transaction is awaiting confirmation from the chain. You may click on the View Transaction button to check it's status.</div>
+			</div>
+		case 'failed':
+			return (
+				<div class='p-4 text-center xl:text-left'>
+					<div class='mb-2 font-bold'>Wallet returned an error!</div>
+					<div class='leading-tight xl:border-l-4 xl:border-l-white/30 px-3 py-1 flex items-center justify-center xl:justify-start text-white/50 text-sm'>{txn.value.error.message}</div>
+					<div class='text-sm mt-2'>Check that your inputs are correct and click Send again.</div>
+				</div>
+			)
+		default:
+			// handle other states on transaction page
+			return null
+	}
+}
+
+const SendActions = () => {
+	const account = accountStore
+	const txn = sendTransactionStore
+
+	switch (account.value.status) {
+		case 'busy':
+			return <div class='px-4 py-2 hover:bg-white/10 border w-full'>Connecting...</div>
+		case 'connected':
+			switch (txn.value.status) {
+				case 'composing':
+					return <button type='submit' class='px-4 py-2 hover:bg-white/10 border w-full'>Send</button>
+				case 'signed':
+					return <button type='submit' class='px-4 py-2 hover:bg-white/10 border w-full'>View Transaction</button>
+				default:
+					return <button type='submit' class='px-4 py-2 bg-white/10 text-white/20 w-full' disabled>Send</button>
+			}
+		case 'disconnected':
+			return <button type='submit' class='px-4 py-2 hover:bg-white/10 border w-full'>Connect Wallet</button>
+		case 'rejected':
+			return <div class='px-4 py-2 bg-red-400/10 border border-red-400/50 text-white/50 text-center cursor-not-allowed'>Unable to connect to wallet!</div>
+	}
+}
+
 
 const TokenSelectField = () => {
 
---
+++ app/ts/store/transaction.ts
@@ -0,0 +1,109 @@
+import { signal } from '@preact/signals'
+import { ethers } from 'ethers'
+import { assertsExternalProvider } from '../library/utilities.js'
+import { TransactionReceipt, TransactionResponse } from '../types.js'
+
+export type SendFnInput = {
+	amount: string
+	to: string
+}
+
+export type TransactionIdle = {
+	status: 'idle'
+	fetch: (hash: string) => void
+	compose: () => void
+}
+
+export type TransactionComposing = {
+	status: 'composing'
+	data: SendFnInput
+	send: () => void
+}
+
+export type TransactionSigning = {
+	status: 'signing'
+	readonly data: SendFnInput
+}
+
+export type TransactionSigned = {
+	status: 'signed'
+	transaction: TransactionResponse
+	fetch: (hash: string) => void
+	readonly data: SendFnInput
+}
+
+export type TransactionConfirming = {
+	status: 'confirming'
+	transaction: TransactionResponse
+	readonly data: SendFnInput
+}
+
+export type TransactionConfirmed = {
+	status: 'confirmed'
+	transaction: TransactionResponse
+	receipt: TransactionReceipt
+	readonly data: SendFnInput
+}
+
+export type TransactionFailed = {
+	status: 'failed'
+	error: Error
+	reset: (clearData?: boolean) => void
+	data: SendFnInput
+}
+
+export type TransactionStore = TransactionComposing | TransactionSigning | TransactionSigned | TransactionConfirming | TransactionConfirmed | TransactionFailed | TransactionIdle
+
+const storeDefaults: TransactionIdle = { status: 'idle', fetch, compose }
+const store = signal<TransactionStore>(storeDefaults)
+export const sendTransactionStore = store
+
+async function send() {
+	if (store.value.status !== 'composing') return
+	const { data } = store.value
+
+	try {
+		assertsExternalProvider(window.ethereum)
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		const signer = provider.getSigner()
+		const value = ethers.utils.parseEther(data.amount)
+		const to = ethers.utils.getAddress(data.to)
+		store.value = { status: 'signing', data }
+		const transaction = await signer.sendTransaction({ to, value })
+		store.value = { status: 'signed', transaction, fetch, data }
+	} catch (exception) {
+		let error = new Error(`Unknown error (${exception})`)
+
+		if (exception instanceof Error) {
+			const parsedMessage = exception.message.replace(/ \(.*\)/, '')
+			const shortMessage = parsedMessage[0].toUpperCase() + parsedMessage.slice(1)
+			error = new Error(shortMessage)
+		}
+
+		store.value = { status: 'failed', error, data, reset }
+	}
+}
+
+async function fetch(hash: string) {
+	if (store.value.status !== 'signed') return
+	const { data } = store.value
+	try {
+		assertsExternalProvider(window.ethereum)
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		const transaction = await provider.getTransaction(hash)
+		store.value = { status: 'confirming', transaction, data }
+		const receipt = await transaction.wait()
+		store.value = { status: 'confirmed', transaction, receipt, data }
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+function reset(clearData?: boolean) {
+	if (store.value.status !== 'failed') return
+	store.value = clearData ? storeDefaults : { status: 'composing', data: store.value.data, send }
+}
+
+function compose() {
+	store.value = { status: 'composing', data: { to: '', amount: '' }, send }
+}

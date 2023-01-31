25d3545c3ebc4669eda94ba465eab6374c49d6f4
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-02-01 02:09:05 +0800

    fixed error boundary code issues

--- app/ts/components/ErrorBoundary.tsx
+++ app/ts/components/ErrorBoundary.tsx
@@ -1,26 +1,24 @@
-import { Component, JSX } from 'preact';
+import { Component, JSX, ErrorInfo } from 'preact'
 
 interface Props {
 	children: JSX.Element | JSX.Element[]
 }
 
-interface State {
-	hasError: boolean;
-	error: Error | null;
-}
+type State = { hasError: false } | { hasError: true; error: Error }
 
 class ErrorBoundary extends Component<Props, State> {
 	constructor(props: Props) {
-		super(props);
-		this.state = { hasError: false, error: null };
+		super(props)
+		this.state = { hasError: false }
 	}
 
 	static getDerivedStateFromError(error: Error) {
-		return { hasError: true, error };
+		return { hasError: true, error }
 	}
 
-	componentDidCatch(error: Error, errorInfo: object) {
-		console.error(error, errorInfo);
+	componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+		// connect to logging service
+		console.error(error, errorInfo)
 	}
 
 	render() {
@@ -30,14 +28,14 @@ class ErrorBoundary extends Component<Props, State> {
 					<div class='border border-red-400/20 bg-red-400/5 p-4 text-center max-w-lg'>
 						<div class='text-xl font-bold'>Uh oh, this is unexpected...</div>
 						<div class='text-white/50 text-sm my-4'>Not to worry. Drop by our disord channel and provide us with the hint below so we can help resolve your issue faster. Appreciate your support and look forward to seeing you there.</div>
-						<div class='border border-red-400/20 px-4 py-3 bg-black/10 text-white/80 text-sm'>{this.state.error?.message}</div>
+						<div class='border border-red-400/20 px-4 py-3 bg-black/10 text-white/80 text-sm'>{this.state.error.message}</div>
 					</div>
 				</div>
-			);
+			)
 		}
 
-		return this.props.children;
+		return this.props.children
 	}
 }
 
-export default ErrorBoundary;
+export default ErrorBoundary

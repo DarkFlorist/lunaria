e57139c6e5b6a3aec7bb6bce743557f054552624
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-02-01 02:19:21 +0800

    Added Darkflorist twitter url
    Co-authored-by: KillariDev <kimi.ylilammi@statomaly.com>

--- app/ts/components/Layout.tsx
+++ app/ts/components/Layout.tsx
@@ -58,10 +58,10 @@ const Branding = () => (
 
 const SocialLinks = () => (
 	<div class='flex items-center gap-6 text-white/30'>
-		<a class='text-white/30 hover:text-white' href='/'>
+		<a class='text-white/30 hover:text-white' href='https://github.com/DarkFlorist'>
 			Github
 		</a>
-		<a class='text-white/30 hover:text-white' href='/'>
+		<a class='text-white/30 hover:text-white' href='https://twitter.com/DarkFlorist'>
 			Twitter
 		</a>
 		<a class='text-white/30 hover:text-white' target='' href='https://discord.gg/gU8BNhqj'>

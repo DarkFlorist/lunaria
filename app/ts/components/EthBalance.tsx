94ee77ad22a3cfd4e5b8f556de821e22bdb07206
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-02-02 03:36:28 +0800

    applied eth balance to layout

--- app/ts/components/EthBalance.tsx
+++ app/ts/components/EthBalance.tsx
@@ -1,3 +1,4 @@
+import { ethers } from 'ethers'
 import { useCallback } from 'preact/hooks'
 import { assertUnreachable } from '../library/utilities.js'
 import { accountStore } from '../store/account.js'
@@ -5,11 +6,11 @@ import { useBalanceAsync } from '../store/balance.js'
 import * as Icon from './Icon/index.js'
 
 export const EthBalance = () => {
-	const ethBalance = useBalanceAsync()
+	const balanceAsync = useBalanceAsync()
 
 	const checkBalance = useCallback(() => {
-		if (accountStore.value.status !== 'connected' || ethBalance.state !== 'inactive') return
-		ethBalance.checkEthBalance()
+		if (accountStore.value.status !== 'connected' || balanceAsync.state !== 'inactive') return
+		balanceAsync.checkEthBalance()
 	}, [accountStore.value.status])
 
 	if (accountStore.value.status === 'disconnected')
@@ -20,14 +21,14 @@ export const EthBalance = () => {
 					<div>
 						<button class='font-bold text-white/80 hover:text-white' onClick={accountStore.value.connect}>
 							Connect wallet
-						</button>{' '}
+						</button>
 						<span class='text-white/50'>to view balance</span>
 					</div>
 				</div>
 			</div>
 		)
 
-	switch (ethBalance.state) {
+	switch (balanceAsync.state) {
 		case 'inactive':
 			checkBalance()
 			return null
@@ -45,11 +46,13 @@ export const EthBalance = () => {
 			)
 
 		case 'resolved':
+			const balance = ethers.utils.formatEther(balanceAsync.balance)
+
 			return (
 				<div class='flex flex-col items-center justify-center md:items-start md:gap-1 mb-2'>
 					<div class='uppercase text-white/50 text-xs md:text-sm flex'>balance (ETH)</div>
 					<div class='flex items-center gap-2'>
-						<div class='text-lg font-bold capitalize'>{ethBalance.balance} ETH</div>
+						<div class='text-lg font-bold capitalize'>{balance} ETH</div>
 						<button type='button' class='focus:outline-none focus:bg-white/10 focus:ring-1 focus:ring-white rounded' title='Refresh' onClick={checkBalance}>
 							<Icon.Refresh />
 						</button>
@@ -60,6 +63,6 @@ export const EthBalance = () => {
 		case 'rejected':
 			return <div>Error</div>
 		default:
-			assertUnreachable(ethBalance)
+			assertUnreachable(balanceAsync)
 	}
 }
--- app/ts/components/Layout.tsx
+++ app/ts/components/Layout.tsx
@@ -1,7 +1,7 @@
 import { ComponentChildren, JSX } from 'preact'
 import { removeNonStringsAndTrim } from '../library/utilities.js'
-import * as Icon from './Icon/index.js'
 import { ConnectToWallet } from './ConnectToWallet.js'
+import { EthBalance } from './EthBalance.js'
 
 type LayoutComponentProps = JSX.HTMLAttributes<HTMLDivElement> & { children: ComponentChildren }
 
@@ -10,9 +10,7 @@ export const Page = ({ children, class: classString }: LayoutComponentProps) => 
 	return (
 		<div class='fixed bg-main inset-0'>
 			<div class='fixed inset-0 bg-polka'></div>
-			<div class={classNames}>
-				{children}
-			</div>
+			<div class={classNames}>{children}</div>
 		</div>
 	)
 }
@@ -27,18 +25,18 @@ export const Header = () => {
 }
 
 export const Body = ({ children }: { children: ComponentChildren }) => {
-	return <div class='[grid-area:body] grid gap-6 [grid-template-areas:_"essentials"_"main"_"aside"] md:[grid-template-areas:_"main_essentials"_"main_aside"] md:grid-cols-[3fr_2fr] xl:grid-cols-[5fr_2fr] grid-rows-[minmax(min-content,max-content)_1fr] px-6'>
-		<div class='[grid-area:essentials]'>
-			<ActiveNetwork />
-			<EthBalance />
+	return (
+		<div class='[grid-area:body] grid gap-6 [grid-template-areas:_"essentials"_"main"_"aside"] md:[grid-template-areas:_"main_essentials"_"main_aside"] md:grid-cols-[3fr_2fr] xl:grid-cols-[5fr_2fr] grid-rows-[minmax(min-content,max-content)_1fr] px-6'>
+			<div class='[grid-area:essentials]'>
+				<ActiveNetwork />
+				<EthBalance />
+			</div>
+			<div class='[grid-area:main]'>{children}</div>
+			<div class='[grid-area:aside]'>
+				<Support />
+			</div>
 		</div>
-		<div class='[grid-area:main]'>
-			{children}
-		</div>
-		<div class='[grid-area:aside]'>
-			<Support />
-		</div>
-	</div>
+	)
 }
 
 export const Footer = () => {
@@ -78,18 +76,6 @@ const Support = () => {
 		</div>
 	)
 }
-
-const EthBalance = () => (
-	<div class='flex flex-col items-center justify-center md:items-start md:gap-1 mb-2'>
-		<div class='uppercase text-white/50 text-xs md:text-sm'>balance (ETH)</div>
-		<div class='flex items-center gap-2'>
-			<div class='text-lg font-bold capitalize'>100.0159 ETH</div>
-			<button type='button' class='focus:outline-none focus:bg-white/10 focus:ring-1 focus:ring-white rounded' title='Refresh' onClick={() => { }}>
-				<Icon.Refresh />
-			</button>
-		</div>
-	</div>
-)
 
 const ActiveNetwork = () => (
 	<div class='flex flex-col items-center justify-center md:items-start md:gap-1 mb-2'>

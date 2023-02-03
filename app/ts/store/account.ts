ac472c5362f5e1900f6664ebf23856b0d190c6ff
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-02-04 00:01:42 +0800

    update account  store

--- app/ts/store/account.ts
+++ app/ts/store/account.ts
@@ -1,17 +1,23 @@
-import { useSignal } from '@preact/signals'
+import { Signal, useSignal } from '@preact/signals'
 import { ethers } from 'ethers'
 import { EthereumJsonRpcError } from '../library/exceptions.js'
-import { AsyncProperty, useAsyncState } from '../library/preact-utilities.js'
+import { AsyncState, useAsyncState } from '../library/preact-utilities.js'
 import { assertsExternalProvider } from '../library/utilities.js'
 
-export type Account =
+type ConnectAsync = {
+	value: AsyncState<unknown>['value']
+	dispatch: () => void
+	reset: AsyncState<unknown>['reset']
+}
+
+type Account =
 	| {
 			status: 'connected'
 			address: string
 	  }
 	| {
 			status: 'disconnected'
-			useAsyncConnectState: () => readonly [AsyncProperty<string>, () => void, () => void]
+			connect: ConnectAsync
 			ensureConnected: () => Promise<string>
 	  }
 	| {
@@ -19,30 +25,33 @@ export type Account =
 			error: EthereumJsonRpcError | Error
 	  }
 
-export function createAccountStore() {
-	async function ensureConnected() {
+export type AccountStore = Signal<Account>
+export function createAccountStore(): AccountStore {
+	const { value, waitFor, reset } = useAsyncState()
+
+	const ensureConnected = async () => {
 		assertsExternalProvider(window.ethereum)
 		const provider = new ethers.providers.Web3Provider(window.ethereum)
 		const signer = provider.getSigner()
 		return await signer.getAddress()
 	}
 
-	const useAsyncConnectState = () => {
-		const [subject, resolve, reset] = useAsyncState<string>()
-
-		const connect = () => {
-			resolve(async () => {
+	const connect = {
+		value,
+		dispatch: () => {
+			waitFor(async () => {
 				assertsExternalProvider(window.ethereum)
 				const provider = new ethers.providers.Web3Provider(window.ethereum)
 				await provider.send('eth_requestAccounts', [])
 				const signer = provider.getSigner()
-				return await signer.getAddress()
+				const address = await signer.getAddress()
+				accountStore.value = { status: 'connected', address }
 			})
-		}
-
-		return [subject, connect, reset] as const
+		},
+		reset,
 	}
 
-	const accountDefaults = { status: 'disconnected', useAsyncConnectState, ensureConnected } as const
-	return useSignal<Account>(accountDefaults)
+	const accountDefaults = { status: 'disconnected', connect, ensureConnected } as const
+	const accountStore = useSignal<Account>(accountDefaults)
+	return accountStore
 }

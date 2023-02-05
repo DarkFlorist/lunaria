ff46645a0b0225aa6fab99db961fd3ec85b347a2
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-02-05 18:24:05 +0800

    moved splash to account context

---
+++ app/ts/components/AccountContext.tsx
@@ -0,0 +1,32 @@
+import { ComponentChildren, createContext } from 'preact'
+import { useContext, useEffect } from 'preact/hooks'
+import { AccountStore } from '../store/account'
+
+const AccountContext = createContext<AccountStore | undefined>(undefined)
+export const AccountProvider = ({ children, store }: { children: ComponentChildren; store: AccountStore }) => {
+	useEffect(() => {
+		if (store.value.status !== 'disconnected') return
+		store.value.reconnect.dispatch()
+	}, [])
+
+	executeSplashExit()
+
+	return <AccountContext.Provider value={store}>{children}</AccountContext.Provider>
+}
+
+export function useAccountStore() {
+	const context = useContext(AccountContext)
+	if (!context) throw new Error('useAccountStore can only be used within a child of AccountProvider')
+	return context
+}
+
+function executeSplashExit() {
+	useEffect(() => {
+		const selectorClassName = '.splash-screen'
+		const selectorHiddenClassName = 'splash-screen--off'
+
+		const element = document.querySelector(selectorClassName)
+		if (!element || element.classList.contains(selectorHiddenClassName)) return
+		element.classList.add(selectorHiddenClassName)
+	}, [])
+}
--- app/ts/store/account.ts
+++ app/ts/store/account.ts
@@ -18,7 +18,7 @@ type Account =
 	| {
 			status: 'disconnected'
 			connect: ConnectAsync
-			ensureConnected: () => Promise<string>
+			reconnect: ConnectAsync
 	  }
 	| {
 			status: 'failed'
@@ -29,11 +29,17 @@ export type AccountStore = Signal<Account>
 export function createAccountStore(): AccountStore {
 	const { value, waitFor, reset } = useAsyncState()
 
-	const ensureConnected = async () => {
-		assertsExternalProvider(window.ethereum)
-		const provider = new ethers.providers.Web3Provider(window.ethereum)
-		const signer = provider.getSigner()
-		return await signer.getAddress()
+	const reconnect = {
+		signal: value,
+		dispatch: () =>
+			waitFor(async () => {
+				assertsExternalProvider(window.ethereum)
+				const provider = new ethers.providers.Web3Provider(window.ethereum)
+				const signer = provider.getSigner()
+				const address = await signer.getAddress()
+				accountStore.value = { status: 'connected', address }
+			}),
+		reset,
 	}
 
 	const connect = {
@@ -51,7 +57,7 @@ export function createAccountStore(): AccountStore {
 		reset,
 	}
 
-	const accountDefaults = { status: 'disconnected', connect, ensureConnected } as const
+	const accountDefaults = { status: 'disconnected', connect, reconnect } as const
 	const accountStore = useSignal<Account>(accountDefaults)
 	return accountStore
 }

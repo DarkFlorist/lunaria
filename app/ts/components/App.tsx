4ee9a8a0580d7dfa385356e8e94f0f1740e42514
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-02-09 15:13:35 +0800

    moved context higher up the component tree

--- app/ts/components/App.tsx
+++ app/ts/components/App.tsx
@@ -4,17 +4,21 @@ import { Route, Router } from './HashRouter.js'
 import { SendEthPage } from './SendEthPage.js'
 import { TransactionPage } from './TransactionPage.js'
 import { TransactionDetailsPage } from './TransactionDetailsPage.js'
+import { BalanceProvider, createBalanceStore } from '../context/BalanceContext.js'
 
 export function App() {
 	const accountStore = createAccountStore()
+	const balanceStore = createBalanceStore()
 
 	return (
 		<AccountProvider store={accountStore}>
-			<Router>
-				<Route path='#tx' component={TransactionPage} />
-				<Route path='#tx/:transaction_hash' component={TransactionDetailsPage} />
-				<Route path='/' component={SendEthPage} />
-			</Router>
+			<BalanceProvider store={balanceStore}>
+				<Router>
+					<Route path='#tx' component={TransactionPage} />
+					<Route path='#tx/:transaction_hash' component={TransactionDetailsPage} />
+					<Route path='/' component={SendEthPage} />
+				</Router>
+			</BalanceProvider>
 		</AccountProvider>
 	)
 }

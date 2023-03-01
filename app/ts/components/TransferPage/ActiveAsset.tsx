fb28e7b707f2739dfce32353f09ce49afcc61b55
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-03-02 04:05:18 +0800

    mobile close menu on select asset

--- app/ts/components/TransferPage/ActiveAsset.tsx
+++ app/ts/components/TransferPage/ActiveAsset.tsx
@@ -1,4 +1,3 @@
-import { ethers } from 'ethers'
 import { useEthereumNetwork } from '../../context/EthereumProvider.js'
 import { useTransfer } from '../../context/Transfer.js'
 import { TokenIcon } from '../Icon/Tokens/index.js'
@@ -104,7 +103,6 @@ const AssetDescription = ({ text }: { text: string }) => {
 
 const CopyAddressButton = ({ address }: { address: string }) => {
 	const network = useEthereumNetwork()
-
 	const handleClick = () => {
 		// TODO: copy address to clipboard
 		console.log(address)

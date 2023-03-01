040bb1fb27781b0062b028d32b4d3780b8637bb6
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-03-02 02:46:32 +0800

    change title to Transfer Asset from Send WETH

--- app/ts/components/TransferPage/index.tsx
+++ app/ts/components/TransferPage/index.tsx
@@ -29,7 +29,7 @@ export const TransferPage = () => {
 					<div style={{ gridArea: 'main' }}>
 						<div class='border border-white/20 mx-4 md:ml-0'>
 							<TransferForm>
-								<div class='px-4 py-2'>Send WETH</div>
+								<div class='px-4 py-2'>Transfer Asset</div>
 								<ActiveAssetDetail onChange={() => (mobileVisibility.value = true)} />
 								<div class='grid [grid-template-areas:_"form"_"review"_"action"_"support"] lg:[grid-template-areas:_"form_review"_"action_review"_"action_support"] lg:grid-cols-[3fr_minmax(0,2fr)] grid-rows-[minmax(min-content,auto)_minmax(min-content,auto)] gap-y-6 gap-x-6 px-6 py-6'>
 									<div style={{ gridArea: 'form' }}>

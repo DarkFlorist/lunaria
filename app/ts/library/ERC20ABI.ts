505cfe5072ac9dd8dce91a18d04f0aedc4addde0
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-03-14 02:18:12 +0800

    add event string functions to abi

--- app/ts/library/ERC20ABI.ts
+++ app/ts/library/ERC20ABI.ts
@@ -3,5 +3,9 @@ export const ERC20ABI = [
 	'function symbol() view returns (string)',
 	'function decimals() view returns (uint8)',
 	'function balanceOf(address account) view returns (uint256)',
-	'function transfer(address recipient, uint256 amount) returns (bool)'
+	'function transfer(address recipient, uint256 amount) returns (bool)',
+	'function approve(address spender, uint256 amount) returns (bool)',
+	'function allowance(address owner, address spender) view returns (uint256)',
+	'event Transfer(address indexed from, address indexed to, uint256 value)',
+	'event Approval(address indexed owner, address indexed spender, uint256 value)'
 ]

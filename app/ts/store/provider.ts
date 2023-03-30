cd99db0689936d56e86cb2a03051feaaddd0951a
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-03-30 13:58:21 +0800

    updated provider function

--- app/ts/store/provider.ts
+++ app/ts/store/provider.ts
@@ -1,45 +1,53 @@
-import { effect, signal } from '@preact/signals'
+import { signal } from '@preact/signals'
 import { ethers } from 'ethers'
-import { assertsWithEthereum } from '../library/ethereum.js'
-import { WalletError } from '../library/exceptions.js'
-import { Web3Provider } from '../types.js'
+import { assertsWithEthereum } from '../library/ethereum'
+import { Notice, useNotice } from './notice'
 
-const provider = signal<Web3Provider | undefined>(undefined)
+const provider = signal<ethers.providers.Web3Provider | undefined>(undefined)
+
+const createChainChangeListener = (handler: (chainHex?: string) => void) => {
+	return {
+		subscribe: () => {
+			assertsWithEthereum(window)
+			window.ethereum.addListener('chainChanged', handler)
+		},
+		unsubscribe: () => {
+			assertsWithEthereum(window)
+			window.ethereum.removeListener('chainChanged', handler)
+		},
+	}
+}
+
+const getBrowserProvider = (chainHex?: string) => {
+	assertsWithEthereum(window)
+	const newNetwork = chainHex ? ethers.providers.getNetwork(chainHex) : undefined
+	provider.value = new ethers.providers.Web3Provider(window.ethereum, newNetwork)
+	return provider.value
+}
+
+const chainChangedObserver = createChainChangeListener(getBrowserProvider)
 
 export function useProviders() {
-	const getbrowserProvider = () => {
-		if (provider.value !== undefined) return provider.value
-
-		try {
-			assertsWithEthereum(window)
-			provider.value = new ethers.providers.Web3Provider(window.ethereum)
-			return provider.value
-		} catch (error) {
-			if (error instanceof WalletError) {
-				throw error
-			}
-
-			const errorMessage = typeof error === 'string' ? error : error instanceof Error ? error.message : 'An unknown error occurred.'
-			throw new Error(errorMessage)
-		}
-	}
-
 	return {
-		browserProvider: provider,
-		getbrowserProvider,
+		// only initialize provider when called, avoid calling this function on page load
+		get browserProvider() {
+			const { notices } = useNotice()
+
+			if (provider.value !== undefined) return provider.value
+
+			try {
+				chainChangedObserver.subscribe()
+				return getBrowserProvider()
+			} catch (error) {
+				const message = error instanceof Error ? error.message : 'An unknown error occurred.'
+				const notice: Notice = {
+					id: notices.value.length + 1,
+					title: 'Important',
+					message,
+				}
+				notices.value = [...notices.value, notice]
+				throw message
+			}
+		},
 	}
 }
-
-const handleChainChange = async () => {
-	removeChainChangeListener()
-
-	// reinitialize provider
-	assertsWithEthereum(window)
-	provider.value = new ethers.providers.Web3Provider(window.ethereum)
-}
-
-const removeChainChangeListener = effect(() => {
-	if (provider.value === undefined) return
-	assertsWithEthereum(window)
-	window.ethereum.on('chainChanged', handleChainChange)
-})

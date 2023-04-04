eb2619595125dfcce17ce3bdaa1ce89738649e94
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-04-04 11:00:22 +0800

    remove test

--- app/ts/components/App.tsx
+++ app/ts/components/App.tsx
@@ -6,10 +6,8 @@ import { AccountProvider } from '../context/Account.js'
 import { TransferPage } from './TransferPage/index.js'
 import { TransactionDetails } from './TransactionDetailsPage.js'
 import { Notices } from './Notice.js'
-import { useNotice } from '../store/notice.js'
 
 export function App() {
-	const { notify } = useNotice()
 	return (
 		<>
 			<EthereumProvider>
@@ -23,9 +21,6 @@ export function App() {
 					</BalanceProvider>
 				</AccountProvider>
 			</EthereumProvider>
-			<button class='fixed bottom-10 right-10 bg-white px-3 py-2' onClick={() => notify({ title: 'Notice', message: 'Hello' })}>
-				Test notice
-			</button>
 			<Notices />
 		</>
 	)

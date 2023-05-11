da1e99c296b23941dd5c42239f74280ac10e87e4
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-05-11 13:33:01 +0800

    remove demo pages (#104)

--- app/ts/components/App.tsx
+++ app/ts/components/App.tsx
@@ -2,16 +2,12 @@ import { Route, Router } from './HashRouter.js'
 import { TransferPage } from './TransferPage/index.js'
 import { AssetsPage } from './AssetsPage/index.js'
 import { Notices } from './Notice.js'
-import { DemoPage } from './Demo.js'
-import { LayoutDemo } from './DefaultLayout/Demo.js'
 import { SplashScreen } from './SplashScreen.js'
 
 export function App() {
 	return (
 		<SplashScreen>
 			<Router>
-				<Route path='#test' component={DemoPage} />
-				<Route path='#layout' component={LayoutDemo} />
 				<Route path='#assets' component={AssetsPage} />
 				<Route path='/' component={TransferPage} />
 			</Router>

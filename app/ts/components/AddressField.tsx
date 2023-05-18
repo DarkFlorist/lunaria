00311c419ac43040ca0233468b629bcc07375595
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-05-18 14:54:22 +0800

    add busy state on form

--- app/ts/components/AddressField.tsx
+++ app/ts/components/AddressField.tsx
@@ -1,8 +1,8 @@
-import { useSignal } from "@preact/signals"
-import { useRef } from "preact/hooks"
-import { JSX } from "preact/jsx-runtime"
-import { removeNonStringsAndTrim } from "../library/utilities.js"
-import * as Icon from "./Icon/index.js"
+import { useSignal } from '@preact/signals'
+import { useRef } from 'preact/hooks'
+import { JSX } from 'preact/jsx-runtime'
+import { removeNonStringsAndTrim } from '../library/utilities.js'
+import * as Icon from './Icon/index.js'
 
 type Props = {
 	label: string
@@ -10,6 +10,7 @@ type Props = {
 	value: string
 	onInput: (amount: string) => void
 	onClear: () => void
+	disabled?: boolean
 }
 
 export const AddressField = (props: Props) => {
@@ -38,14 +39,14 @@ export const AddressField = (props: Props) => {
 	}
 
 	return (
-		<div class={removeNonStringsAndTrim(baseClasses.root, isValid.value === false ? 'border-red-400 focus-within:border-red-400' : 'border-white/50 focus-within:border-white/90')}>
+		<div class={removeNonStringsAndTrim(baseClasses.root, isValid.value === false ? 'border-red-400 focus-within:border-red-400' : 'border-white/50 focus-within:border-white/90', props.disabled && 'opacity-50')}>
 			<div class='grid grid-cols-[1fr,auto] items-center h-16'>
 				<div class='grid px-4'>
 					<label class='text-sm text-white/50 leading-tight'>{props.label}</label>
-					<input ref={inputRef} placeholder={props.placeholder} pattern='^0x[0-9A-Fa-f]{40}$' class={baseClasses.field} type='text' value={props.value} onInput={handleInput} required />
+					<input ref={inputRef} placeholder={props.placeholder} pattern='^0x[0-9A-Fa-f]{40}$' class={baseClasses.field} type='text' value={props.value} onInput={handleInput} disabled={props.disabled} required />
 				</div>
 				{props.value !== '' && (
-					<button type='button' class='mx-2 p-2 outline-none border border-transparent focus:border-white text-sm' onClick={handleClear}>
+					<button type='button' class='mx-2 p-2 outline-none border border-transparent focus:border-white text-sm disabled:opacity-50' onClick={handleClear} disabled={props.disabled}>
 						<Icon.Xmark />
 					</button>)}
 

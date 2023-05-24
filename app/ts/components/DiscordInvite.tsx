810220e30c1b51fb8393c8bd281e50a38b24637f
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-05-24 23:17:31 +0800

    Update app/ts/components/DiscordInvite.tsx
    Co-authored-by: KillariDev <13102010+KillariDev@users.noreply.github.com>

--- app/ts/components/DiscordInvite.tsx
+++ app/ts/components/DiscordInvite.tsx
@@ -5,7 +5,7 @@ export const DiscordInvite = () => {
 			<div class='p-4 text-sm bg-white/5 border border-white/10'>
 				<div class='text-white/50 mb-4'>Join our discord channel to get support from our active community members and stay up-to-date with the latest news, events, and announcements.</div>
 				<a class='block px-3 py-2 text-center font-bold hover:bg-white/10' href='https://discord.gg/BeFnJA5Kjb'>
-					Join TheInterceptor
+					Join The Interceptor Discord
 				</a>
 			</div>
 		</div>

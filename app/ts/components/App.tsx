f003e9989629435d00aebf0122538cdad989d6ab
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-05-27 18:36:52 +0800

    remove test page

--- app/ts/components/App.tsx
+++ app/ts/components/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Router } from './HashRouter.js'
 import { TransferPage } from './TransferPage/index.js'
-import { AssetsPage } from './AssetsPage/index.js'
 import { Notices } from './Notice.js'
 import { SplashScreen } from './SplashScreen.js'
 import { TransactionPage } from './TransactionPage/index.js'
@@ -9,9 +8,6 @@ export function App() {
 	return (
 		<SplashScreen>
 			<Router>
-				<Route path='#assets'>
-					<AssetsPage />
-				</Route>
 				<Route path=''>
 					<TransferPage />
 				</Route>

65166575b4f23a383f21c6d0054775f013795578
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-06-04 15:24:11 +0800

    readable recent transfers

--- app/ts/components/RecentTransfers.tsx
+++ app/ts/components/RecentTransfers.tsx
@@ -1,5 +1,6 @@
 import { useComputed } from '@preact/signals'
 import { useRecentTransfers } from '../store/recent-transfers.js'
+import { TimeAgo } from './TimeAgo.js'
 
 export const RecentTransfers = () => {
 	const { recentTxns } = useRecentTransfers()
@@ -13,12 +14,11 @@ export const RecentTransfers = () => {
 			<div class='text-white/30 text-sm mb-2'>Recent Transfers</div>
 			<div>
 				{sortedTxns.value.map(txn => {
-					const date = new Date(txn.date)
-					const dateTime = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
+					const timeStamp = new Date(txn.date).getTime()
 					return (
 						<a class='block bg-white/10 px-4 py-3 mb-1 hover:bg-white/20' href={`#tx/${txn.hash}`}>
-							<div class='overflow-hidden text-ellipsis whitespace-nowrap'>{txn.hash}</div>
-							<div class='text-sm text-white/50'>{dateTime}</div>
+							<div class='overflow-hidden text-ellipsis whitespace-nowrap'>{txn.isToken ? 'Token' : 'Ether'} Transfer</div>
+							<div class='text-sm text-white/50'><TimeAgo since={timeStamp} /></div>
 						</a>
 					)
 				})}

466c3428b9416a37525a3b829a49a1bba3f27089
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-08-16 18:50:09 +0800

    fix style not correctly formatted on generate (#191)

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	singleQuote: true,
+	jsxSingleQuote: true,
+	useTabs: true,
+	semi: false,
+	proseWrap: "never",
+	singleAttributePerLine: false,
+	printWidth: 999,
+	arrowParens: "avoid",
+}

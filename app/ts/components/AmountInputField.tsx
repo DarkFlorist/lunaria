6216721dbda07b2d88f748a6df95f211175693b2
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-09-07 13:53:11 +0800

    rework component style handling

--- app/ts/components/AmountInputField.tsx
+++ app/ts/components/AmountInputField.tsx
@@ -1,7 +1,9 @@
 import { Signal, useComputed } from "@preact/signals"
 import { formatUnits, Numeric, parseUnits } from "ethers"
+import { ComponentChildren } from "preact"
 import { useRef } from "preact/hooks"
 import { JSX } from "preact/jsx-runtime"
+import { removeNonStringsAndTrim } from "../library/utilities.js"
 import * as Icon from "./Icon/index.js"
 
 type AmountFieldProps = {
@@ -9,6 +11,7 @@ type AmountFieldProps = {
 	signalValue: Signal<bigint | undefined>,
 	decimals?: string | Numeric
 	onSetMaxValue: () => void
+	disabled?: true
 }
 
 export const AmountField = (props: AmountFieldProps) => {
@@ -43,16 +46,30 @@ export const AmountField = (props: AmountFieldProps) => {
 	}
 
 	return (
-		<div class='relative grid grid-cols-[1fr,min-content] grid-rows-[min-content,min-content] grid-flow-col-dense gap-x-3 py-3 px-4'>
+		<Wrapper>
 			<label for={props.id} class='text-sm text-white/50'>Amount</label>
-			<input ref={inputRef} required id={props.id} placeholder="1.00" value={displayValue.value} inputMode='numeric' pattern='^\s*(\d+(\.\d+)?)?\s*$' onInput={updateValue} class='h-6 bg-transparent outline-none placeholder:text-white/20 peer' />
-			<div class='row-span-2 items-center justify-center hidden peer-placeholder-shown:flex'>
-				<button type='button' onClick={clearValue} class='px-2 h-8 flex items-center justify-center text-xs outline-none uppercase text-white/50 border border-white/50 focus:border-white focus:text-white'>max</button>
-			</div>
-			<div class='row-span-2 flex items-center justify-center peer-placeholder-shown:hidden'>
-				<button type='button' onClick={props.onSetMaxValue} class='w-8 h-8 flex items-center justify-center text-sm outline-none focus:border focus:border-white'><Icon.Xmark /></button>
-			</div>
-			<div class='pointer-events-none absolute inset-0 border border-white/50 peer-invalid:border-red-400 peer-invalid:peer-placeholder-shown:border-white/50 peer-invalid:peer-placeholder-shown:peer-focus:border-white'></div>
+			<input ref={inputRef} disabled={props.disabled} required id={props.id} placeholder="1.00" value={displayValue.value} inputMode='numeric' pattern='^\s*(\d+(\.\d+)?)?\s*$' onInput={updateValue} class='h-6 bg-transparent outline-none placeholder:text-white/20 peer disabled:border-white/50 disabled:text-white/50' />
+			<Button onClick={props.onSetMaxValue} class='hidden peer-placeholder-shown:flex [&>button]:px-2'>max</Button>
+			<Button onClick={clearValue} class='flex peer-placeholder-shown:hidden [&>button]:w-8'><Icon.Xmark /></Button>
+			<BackDraft />
+		</Wrapper>
+	)
+}
+
+type ButtonProps = {
+	onClick: () => void
+	children: ComponentChildren
+	class?: string
+}
+
+const Button = ({ children, onClick, class: className }: ButtonProps) => {
+	return (
+		<div class={removeNonStringsAndTrim('row-span-2 items-center justify-center', className)}>
+			<button type='button' onClick={onClick} class='h-8 flex items-center justify-center text-xs outline-none uppercase text-white/50 border border-white/50 focus:border-white focus:text-white'>{children}</button>
 		</div>
 	)
 }
+
+const BackDraft = () => <div class='pointer-events-none absolute inset-0 border border-white/50 peer-invalid:border-red-400 peer-invalid:peer-placeholder-shown:border-white/50 peer-invalid:peer-placeholder-shown:peer-focus:border-white'></div>
+
+const Wrapper = ({ children }: { children: ComponentChildren }) => <div class='relative grid grid-cols-[1fr,min-content] grid-rows-2 grid-flow-col-dense gap-x-3 py-3 px-4'>{children}</div>
--- app/ts/components/Test.tsx
+++ app/ts/components/Test.tsx
@@ -11,8 +11,8 @@ export const Test = () => {
 	return (
 		<div class='fixed inset-0 bg-black text-white p-6'>
 			<form onSubmit={sendTransaction}>
-				<AmountField id="send_input" signalValue={amount} />
-				<button type="submit">send</button>
+				<AmountField id="send_input" signalValue={amount} onSetMaxValue={()=> {}} />
+				<button class='px-3 py-2 border mt-2' type="submit">send</button>
 			</form>
 		</div>
 	)
